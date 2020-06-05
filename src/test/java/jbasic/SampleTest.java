10efeb4db22f5b129604e7f1270e78a0b830c1bc
Shinji Miura <40balmung@gmail.com>
2020-06-05 15:06:04 +0900

    modify settings

---
+++ src/test/java/jbasic/SampleTest.java
@@ -0,0 +1,14 @@
+package jbasic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest extends Sample {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
